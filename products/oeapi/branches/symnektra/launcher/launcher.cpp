46ca7e1d978460e7964c6480b7d6dd2b0740699e
astepaniuk <astepaniuk>
2010-07-27 14:41:29 +0000

    <SVN message originally empty>
    Exported from SVN r10652

--- products/oeapi/branches/symnektra/launcher/launcher.cpp
+++ products/oeapi/branches/symnektra/launcher/launcher.cpp
@@ -1,10 +1,9 @@
 // launcher.cpp : Defines the entry point for the application.
 //
 
-#include "stdafx.h"
+#include <windows.h>
 #include <tchar.h>
 
-#include "oeapi_utils.h"
 #include "se_debug.h"
 
 
@@ -14,7 +13,8 @@ int APIENTRY WinMain(HINSTANCE hInstance,
                      int       nCmdShow)
 {
  	HMODULE hOehook = LoadLibrary(_T("oehook.dll"));
-	if(hOehook == NULL) {
+	if(hOehook == NULL)
+    {
 		debug_print(DEBUG_ERROR, _T("Launcher: Cannot load oehook.dll\n"));
 		return 1;
 	}
