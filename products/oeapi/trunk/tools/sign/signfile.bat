2110a9dce965f8d5b5591ac02e1129ae0d1784fe
ibejarano <ibejarano>
2016-01-14 19:39:10 +0000

    Update certificate
    Exported from SVN r18513

--- products/oeapi/trunk/tools/sign/signfile.bat
+++ products/oeapi/trunk/tools/sign/signfile.bat
@@ -1,1 +1,1 @@
-@SignTool sign /f "%~dp0\nektra_code_certificate.p12" /p vf0j43d#5 /t http://timestamp.comodoca.com/authenticode "%~1"
+@SignTool sign /f "%~dp0\nektra_code_certificate.p12" /p dike2361 /t http://timestamp.comodoca.com/authenticode "%~1"
